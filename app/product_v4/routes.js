ccf5a3a832ef00a40f41ba3e8213679defac76f8
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2022-09-19 17:45:29 +0700

    upload8

--- app/product_v4/routes.js
+++ app/product_v4/routes.js
@@ -3,6 +3,13 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads' });
 const productController = require('../product_v4/controller')
 
+router.get('/', (req, res) => {
+    res.send({
+        status: 'Successfully',
+        message: 'Welcome to Express JS Tutorial',
+        nama: 'Mochamad Dwi Pamungkas'
+    })
+});
 router.get('/product', productController.index)
 router.get('/product/:id', productController.view)
 router.post('/product', upload.single('image'), productController.post)
